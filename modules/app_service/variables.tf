04118de5feabdc6626e1375c15df277ef8f3619a
Jeremy Hulick <jeremy.hulick@gmail.com>
2026-01-14 20:03:24 -0700

    remove defaults from vars

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,25 +1,21 @@
 variable "app_service_plan_name" {
   type        = string
   description = "The app service plan name."
-  default     = "employee-app-plan"
 }
 
 variable "app_service_name" {
   type        = string
   description = "The app service name."
-  default     = "employee-app-service"
 }
 
 variable "resource_group_name" {
   type        = string
   description = "The resource group for the app."
-  default     = "employee-app-rg"
 }
 
 variable "location" {
   type        = string
   description = "The location of the app deployment."
-  default     = "westus2"
 }
 
 variable "cosmos_endpoint" {
--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,23 +1,19 @@
 variable "resource_group_name" {
   type        = string
   description = "The resource group for the app."
-  default     = "employee-app-rg"
 }
 
 variable "location" {
   type        = string
   description = "The location of the app deployment."
-  default     = "westus2"
 }
 
 variable "vnet_name" {
   type        = string
   description = "The app virtual network name."
-  default     = "employee-vnet"
 }
 
 variable "address_space" {
   type        = list(any)
   description = "The virtual network address space."
-  default     = ["10.0.0.0/16"]
 }
